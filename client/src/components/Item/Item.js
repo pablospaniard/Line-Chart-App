d07627a0fe5be14a3b347deb267587a1d28d0a25
Pavel Ivanov <pablospaniard@gmail.com>
2018-09-09 12:30:29 +0200

    chart done

--- client/src/components/Item/Item.js
+++ client/src/components/Item/Item.js
@@ -2,47 +2,75 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Grid } from '@material-ui/core'
 import { Line as LineChart } from 'react-chartjs-2'
+import { linearRegression } from 'simple-statistics'
 
 import { Spinner } from '../UI'
 import styles from './Item.scss'
 
 class Item extends Component {
-  onChartButtonClick = e => {
-    console.log(e.target.name)
+  state = {
+    firstLine: null,
+    secondLine: null,
+    thirdLine: null,
+    labels: [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'r',
+      's',
+      't',
+      'q'
+    ]
+  }
+
+  onChartButtonClick = () => {
+    const { dataSet } = this.props
+    const { labels, firstLine } = this.state
+    const array = []
+    const newArray = [[], [], []]
+
+    for (let j = 0; j < 3; j++) {
+      for (let i = 0; i < labels.length; i++) {
+        array.push([i, dataSet[0][i]])
+        newArray[j].push(Object.values(linearRegression(array))[1])
+      }
+    }
+
+    if (Object.values(linearRegression(array)) && firstLine === null) {
+      this.setState({
+        firstLine: newArray[0],
+        secondLine: newArray[1],
+        thirdLine: newArray[2]
+      })
+    }
   }
 
   render() {
     const { dataSet, loading } = this.props
+    const { labels } = this.state
     const data = {
-      labels: [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'r',
-        's',
-        't',
-        'q'
-      ],
+      labels,
       datasets: [
         {
           label: 'First dataset',
           fill: false,
           lineTension: 0.5,
           backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'transparent',
+          showLine: false,
+          borderColor: 'rgba(75,192,192,1)',
           borderCapStyle: 'butt',
           borderDash: [],
           borderDashOffset: 0.0,
@@ -63,7 +91,8 @@ class Item extends Component {
           fill: false,
           lineTension: 0.5,
           backgroundColor: 'rgba(192,192,75,0.4)',
-          borderColor: 'transparent',
+          showLine: false,
+          borderColor: 'rgba(192,192,75,1)',
           borderCapStyle: 'butt',
           borderDash: [],
           borderDashOffset: 0.0,
@@ -84,7 +113,8 @@ class Item extends Component {
           fill: false,
           lineTension: 0.5,
           backgroundColor: 'rgba(192,75,192,0.4)',
-          borderColor: 'transparent',
+          showLine: false,
+          borderColor: 'rgba(192,75,192,1)',
           borderCapStyle: 'butt',
           borderDash: [],
           borderDashOffset: 0.0,
@@ -99,6 +129,75 @@ class Item extends Component {
           pointRadius: 5,
           pointHitRadius: 10,
           data: dataSet[2]
+        },
+        {
+          label: 'First regression',
+          fill: false,
+          hidden: true,
+          lineTension: 0,
+          backgroundColor: 'rgba(219,242,242,0.4)',
+          borderColor: 'rgba(219,242,242, 1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(219,242,242, 1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(219,242,242, 1)',
+          pointHoverBorderColor: 'rgba(219,242,242,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 5,
+          pointHitRadius: 10,
+          spanGaps: true,
+          data: this.state.firstLine
+        },
+        {
+          label: 'Second regression',
+          fill: false,
+          hidden: true,
+          lineTension: 0,
+          backgroundColor: 'rgba(242,242,219,0.4)',
+          borderColor: 'rgba(242,242,219, 1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(242,242,219, 1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(242,242,219, 1)',
+          pointHoverBorderColor: 'rgba(242,242,219,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 5,
+          pointHitRadius: 10,
+          spanGaps: true,
+          data: this.state.secondLine
+        },
+        {
+          label: 'Third regression',
+          fill: false,
+          hidden: true,
+          lineTension: 0,
+          backgroundColor: 'rgba(242,219,242,0.4)',
+          borderColor: 'rgba(242,219,242,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(242,219,242,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(242,219,242,1)',
+          pointHoverBorderColor: 'rgba(242,219,242,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 5,
+          pointHitRadius: 10,
+          spanGaps: true,
+          data: this.state.thirdLine
         }
       ]
     }
@@ -109,14 +208,9 @@ class Item extends Component {
 
     const chartOptions = {
       position: 'bottom',
-      tooltips: {
-        mode: 'label'
-      },
-      elements: {
-        line: {
-          fill: false
-        }
-      }
+      tooltips: { mode: 'label' },
+      elements: { line: { fill: false } },
+      onClick: this.onChartButtonClick
     }
 
     return (
